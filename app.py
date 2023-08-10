46a0dabee574493d55e5bccc25d474164fa229e0
Celio121 <celiopereira751@gmail.com>
2023-08-10 21:43:20 +0000

    first commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import sqlite3
 import sys
 
-def db_create():
+def db_create(): # Creating the table database
     with sqlite3.connect("timelogged.db") as connection:
         cursor = connection.cursor()
         try:
@@ -15,7 +15,7 @@ def db_create():
         except Exception as e:
             print(f"Error creating table: {e}")
 
-def time_in(firstname, lastname):
+def time_in(firstname, lastname): # time in inputting data into database
     with sqlite3.connect("timelogged.db") as connection:
         cursor = connection.cursor()
         current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -27,7 +27,7 @@ def time_in(firstname, lastname):
                     ) VALUES (?, ?, ?, ?)""", (firstname, lastname, True, current_time,))
         connection.commit()
 
-def time_out(firstname, lastname):
+def time_out(firstname, lastname): # time out inputting data into database
     with sqlite3.connect("timelogged.db") as connection:
         cursor = connection.cursor()
         current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -39,23 +39,32 @@ def time_out(firstname, lastname):
                     ) VALUES (?, ?, ?, ?)""", (firstname, lastname, False, current_time,))
         connection.commit()
 
-if __name__ == '__main__':
+if __name__ == '__main__': # Run the code using app.py name
     db_create()
 
-    if len(sys.argv) == 2:
-        choice = sys.argv[1]
-    else:
-        print("Usage: echo <choice>")
-        sys.exit(1)
+    while True: # Selecting the operation which the user is requesting to input their name
+        print("Choose an option:")
+        print("1. Sign In")
+        print("2. Sign Out")
+        print("3. Exit")
 
-    if choice == '1':
-        firstname = input("Enter your first name: ")
-        lastname = input("Enter your last name: ")
-        time_in(firstname, lastname)
-        print("Sign-In recorded.")
+        choice = input("Enter your choice (1, 2, or 3): ")
 
-    elif choice == '2':
-        firstname = input("Enter your first name: ")
-        lastname = input("Enter your last name: ")
-        time_out(firstname, lastname)
-        print("Sign-Out recorded.")
+        if choice == '1': # signing in option
+            firstname = input("Enter your first name: ")
+            lastname = input("Enter your last name: ")
+            time_in(firstname, lastname)
+            print("Sign-In recorded.")
+
+        elif choice == '2': #signing out option
+            firstname = input("Enter your first name: ")
+            lastname = input("Enter your last name: ")
+            time_out(firstname, lastname)
+            print("Sign-Out recorded.")
+
+        elif choice == '3': # exiting the operation
+            print("Exiting...")
+            break  # Exit the loop and end the program
+        
+        else:
+            print("Invalid choice. Please enter either '1', '2', or '3'.")
